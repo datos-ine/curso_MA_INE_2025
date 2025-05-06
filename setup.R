652f7542fc3f376c9c2b0d5c4fcbe929736b0273
Tamara Ricardo <161865777+datos-ine@users.noreply.github.com>
2025-05-06 14:38:11 -0300

    update

--- setup.R
+++ setup.R
@@ -1,21 +1,29 @@
 # opciones globales
 knitr::opts_chunk$set(
-  message = F,
-  warning = F,
   fig.align = "center",
   out.width = "85%",
   dpi = 300
 )
 
-showtext::showtext_opts(dpi = 300)
 
 # paquetes
 pacman::p_load(
-  flextable,
   patchwork,
+  scico,
   janitor,
+  easystats,
+  kableExtra,
   tidyverse
 )
 
 # paleta colorblind-friendly
 pal <- scico::scico(n = 9, palette = "tokyo")
+
+# Formato tabla
+kbl_format <- function(x) {
+ kbl(x) |> 
+  kable_styling(bootstrap_options = c("stripped", "condensed", "hover"),
+                fixed_thead = TRUE,
+                html_font = "Calibri") |>
+  row_spec(0, background = "#2f2e4e", color = "white")
+}
